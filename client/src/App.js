99b02d328a57a6904e410258a0559f6b1cf9dcda
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-07-25 12:01:45 -0400

    code all in for backend contact but need to fix. not working

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import AUTH from "./utils/AUTH";
 import BusinessForm from "./pages/BusinessForm";
 import ContactPage from "./pages/ContactPage";
 import Home from "./pages/Home";
+import SearchResult from "./pages/SearchResult"
 
 
 function App() {
