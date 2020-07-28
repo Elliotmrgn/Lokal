27ef66c2068fa7efe3767325722c03cb5cfcddf0
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-07-27 21:03:45 -0400

    added in search bar and fixed image sizes

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { Link } from 'react-router-dom';
 import { Col } from '../Grid';
+// import NavSearchBar from "../../components/NavSearchBar/NavSearchBar"
 import './Nav.css';
 
 const Nav = (props) => {
@@ -29,7 +30,12 @@ const Nav = (props) => {
       </Col>
       <Col size="md-6 sm-6">
         <div className="float-right">
+        {/* <NavSearchBar />   */}
+
         <Link to="/Contact" id="contact" className="navbar-contact px-4">Contact</Link>
+        
+        <Link to="/BusinessForm" id="register" className="navbar-contact px-4">Register</Link>
+
         {greeting} - <Link to="#" className="logout" onClick={props.logout}>Logout</Link>
         </div>
       </Col>
---
+++ client/src/components/NavSearchBar/NavSearchBar.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import API from "../../utils/API";
+
+function SearchBar() {
+  const [formObject, setFormObject] = useState({});
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormObject({ ...formObject, [name]: value });
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const search = formObject.search;
+    API.findViaSearch(search).then((res) => {
+      console.log(res);
+    });
+  }
+
+  return (
+    <div>
+      <form>
+        <div className="form-group">
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            name="search"
+            placeholder="Search by business name or category"
+            onChange={handleInputChange}
+          />
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={handleFormSubmit}
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
+export default SearchBar;
