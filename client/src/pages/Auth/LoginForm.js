eb79a44d9a7409cc786d17b91b3d44fb840c7868
Samuel Guevara <samsjamin@outlook.com>
2020-08-02 14:45:28 -0400

    updated otp

--- client/src/pages/Auth/LoginForm.js
+++ client/src/pages/Auth/LoginForm.js
@@ -1,68 +1,62 @@
-import React, { useState } from "react";
-import { Redirect, Link } from "react-router-dom";
-import { Container, Row, Col } from "../../components/Grid";
-import { Card } from "../../components/Card";
-import { Input, FormBtn } from "../../components/Form";
+import React, { useState } from 'react';
+import { Redirect, Link } from 'react-router-dom';
+import { Container, Row, Col } from '../../components/Grid';
+import { Card } from '../../components/Card';
+import { Input, FormBtn } from '../../components/Form';
 
-function LoginForm({ login }) {
+function LoginForm({login}) {
   const [userObject, setUserObject] = useState({
-    username: "",
-    password: "",
+    username: '',
+    password: ''
   });
   const [redirectTo, setRedirectTo] = useState(null);
 
-  const handleChange = (event) => {
-    setUserObject({
+	const handleChange = (event) => {
+		setUserObject({
       ...userObject,
-      [event.target.name]: event.target.value,
-    });
-  };
+			[event.target.name]: event.target.value
+		});
+	};
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    login(userObject.username, userObject.password);
-    setRedirectTo("/");
-  };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		login(userObject.username, userObject.password);
+		setRedirectTo('/');
+	};
 
   if (redirectTo) {
-    return <Redirect to={{ pathname: redirectTo }} />;
+    return <Redirect to={{ pathname: redirectTo }} />
   } else {
     return (
-      <main>
-       <Container>
+      <Container>
         <Row>
           <Col size="md-3"></Col>
           <Col size="md-6">
-          <div className="title">Login to lokal</div>
-              <form style={{ marginTop: 50 }}>
-                {/* <label class="form__label" htmlFor="username">Username: </label> */}
+            <Card title="Login to React Reading List">
+              <form style={{marginTop: 10}}>
+                <label htmlFor="username">Username: </label>
                 <Input
                   type="text"
                   name="username"
                   value={userObject.username}
                   onChange={handleChange}
-                  placeholder="username"
-                  class="form__field"
                 />
-                {/* <label class="form__label" htmlFor="password">Password: </label> */}
+                <label htmlFor="password">Password: </label>
                 <Input
                   type="password"
                   name="password"
                   value={userObject.password}
                   onChange={handleChange}
-                  placeholder="password"
-                  class="form__field"
                 />
                 <Link to="/signup">Register</Link>
-                <FormBtn onClick={handleSubmit}>LOGIN</FormBtn>
+                <FormBtn onClick={handleSubmit}>Login</FormBtn>
               </form>
-            {/* </Card> */}
+            </Card>
           </Col>
           <Col size="md-3"></Col>
         </Row>
       </Container>
-      </main>
-    );
+    )
   }
 }
 
