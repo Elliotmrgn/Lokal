53fd1715bf26ea63d6759c746afcb77766fabea9
ColinJohnson747 <60419260+ColinJohnson747@users.noreply.github.com>
2020-08-03 20:19:31 -0400

    fixed a tag

--- client/src/pages/businesspage/profilePgComponents/header.js
+++ client/src/pages/businesspage/profilePgComponents/header.js
@@ -1,18 +1,18 @@
-// code for business name, website and tags 
+// code for business name, website and tags
 import React from "react";
 
 // Bootstrap
 
-
 function headerPF(props) {
-
-    return (
-        <div>
-            <span className="profileName">{props.name}</span>
-            <br></br>
-            <a href="/" className="largeLink">{props.website}</a>
-        </div>
-    )
+  return (
+    <div>
+      <span className="profileName">{props.name}</span>
+      <br></br>
+      <a href={props.website} className="largeLink">
+        {props.website}
+      </a>
+    </div>
+  );
 }
 
 export default headerPF;
