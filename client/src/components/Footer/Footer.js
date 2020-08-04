565d47e707dfd5570d101008e61264f2efb39fe8
ColinJohnson747 <60419260+ColinJohnson747@users.noreply.github.com>
2020-08-03 20:05:41 -0400

    fix

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,36 +1,29 @@
 import React from "react";
 function Footer() {
-    return (
-      <div>
-      <footer 
-      style ={{
-          backgroundColor:"#5B9098",
+  return (
+    <div>
+      <footer
+        style={{
+          backgroundColor: "#5B9098",
           color: "white",
           padding: "30px",
           bottom: "0",
           left: "0",
           width: "100%",
-          flexShrink: "0"
+          flexShrink: "0",
         }}
-        > 
+      >
         <div className="footer-container">
-          <span 
-          id="copyright">&copy; {new Date().getFullYear()} 
-          &nbsp;COPYRIGHT </span>
-          <span 
-          id="contact-link" 
-          onClick={(event) => (window.location.href = "/contact")}>
-          CONTACT </span>
+          <span id="copyright">
+            &copy; {new Date().getFullYear()}
+            &nbsp;COPYRIGHT{" "}
+          </span>
+          <Link to="/contact" id="contact-link" style={{ color: "White" }}>
+            CONTACT
+          </Link>
         </div>
       </footer>
-      </div>
-    );
-  }
-  export default Footer
-
-
-
-
-
-
-
+    </div>
+  );
+}
+export default Footer;
